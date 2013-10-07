0695acad22693c4b337af84f54ad52e142046788
anup <anup@netiapps.com>
2013-10-07 13:43:11 +0530

    commit1343

--- tests/client.php
+++ tests/client.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set("soap.wsdl_cache_enabled", "0");
-$client = new soapClient("http://localhost/wealth07101241/test1/requests/pullServiceList.wsdl");
+$client = new soapClient("http://localhost/wealth07101338/SOAP/requests/pullServiceList.wsdl");
 
 $info = $client->__call('fetchServiceList', array('68'));// Salesorder ID and Invoice No
 print_r($info);
