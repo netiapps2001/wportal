d39b99965cacadc8f828b921bc876665206930dd
roopali <roopali@netiapps.com>
2013-10-15 17:25:57 +0530

    commit1725

--- includes/system/globals.php
+++ includes/system/globals.php
@@ -20,7 +20,7 @@ $PORTALPASS = 'root';
 $errorArray = array();
 
 $PORTALDB = 'wealthportal';
-$VERSIONDIR = 'wealth15101012/';
+$VERSIONDIR = 'wealth15101150/';
 //$SERVERPATH = '192.168.1.10/~anupssh/wealthportal/';
 //$ROOTDIR = $SERVERPATH;
 //$VERSIONDIR = '';
